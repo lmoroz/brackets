6948c0a34d3d229b1e2714c5fdc9ed574b88026c
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-09-23 15:32:23 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const configMap = new Map(bracketsConfig), stack = [];
+
+    str.split('').map(symbol =>
+        stack.length > 0 && configMap.get(stack[stack.length - 1]) === symbol
+        ? stack.pop()
+        : stack.push(symbol),
+    );
+    return stack.length === 0;
+};
